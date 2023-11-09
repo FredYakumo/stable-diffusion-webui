3ebbe31f3658fef737e9bd21f4a3a92b47ecd284
fredyakumo <fredyakumo@gmail.com>
2023-11-09 15:14:36 +0800

    update start script

--- gui.ps1
+++ gui.ps1
@@ -1,6 +1,19 @@
-$env:HTTP_PROXY="http://127.0.0.1:7890"
-$env:HTTPS_PROXY="http://127.0.0.1:7890"
+param(
+    [String]$auth_command = "",
+    [String]$auth_name = "fredyakumo",
+    [String]$auth_password = "123456"
+);
+
+$no_password_port = 7862
+
+$env:HTTP_PROXY = "http://127.0.0.1:7890"
+$env:HTTPS_PROXY = "http://127.0.0.1:7890"
 
 conda activate C:\Users\fredyakumo\.conda\envs\stable-diffusion
 
-python.exe webui.py --xformers --api --autolaunch --listen --enable-insecure-extension-access --gradio-auth fredyakumo:123456
+if ($auth_command -eq "--auth") {
+    python.exe webui.py --xformers --api --autolaunch --listen --enable-insecure-extension-access --gradio-auth "${auth_name}:${auth_password}"
+}
+else {
+    python.exe webui.py --xformers --api --autolaunch --listen --enable-insecure-extension-access --port $no_password_port
+}
